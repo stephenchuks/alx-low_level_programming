97bd5c853ead2cd62ea00103d0900dd820424e95
stephenchuks <stephenchuks@uopeople.edu>
2022-06-02 04:13:55 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * <stdio> header
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
