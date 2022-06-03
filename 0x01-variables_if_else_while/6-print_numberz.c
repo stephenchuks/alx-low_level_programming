5d81a2f5f06f9962852735c0fd8d052b77e81261
stephenchuks <stephenchuks@uopeople.edu>
2022-06-03 10:32:40 -0700

    hot fix

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,19 @@
+
 #include <stdio.h>
+
 /**
- * main - print if the number is postive, zero, or negative
- *
- * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
- * Return: 0
- */
-	int main(void)
+* main - Print all single digit numbers using only putchar
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-	char c;
-	for (c = '0'; c <= '9'; c++)
-{
-	putchar(c);
-}
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	putchar(digit + '0');
+
 	putchar('\n');
+
 	return (0);
 }
