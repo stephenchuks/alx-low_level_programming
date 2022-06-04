baee750f3d0b46b472d16a9e10a7132600a3520b
stephenchuks <104122101+stephenchuks@users.noreply.github.com>
2022-06-04 08:56:42 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - print if the number is postive, zero, or negative
+ * main - prints all single digit numbers of base 10 starting from 0, followed by a new line
  *
  * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
+ * prints all single digit numbers of base 10 starting from 0, followed by a new line
  * Return: 0
  */
 int main(void)
