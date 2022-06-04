3f68921d3ce1e8f8aff00e453927bce405bcf700
stephenchuks <104122101+stephenchuks@users.noreply.github.com>
2022-06-04 08:49:04 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - print if the number is postive, zero, or negative
+ * main - prints the lowercase alphabet in reverse, followed by a new line
  *
  * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
+ * this program prints "prints the lowercase alphabet in reverse, followed by a new line"
  * Return: 0
  */
 int main(void)
