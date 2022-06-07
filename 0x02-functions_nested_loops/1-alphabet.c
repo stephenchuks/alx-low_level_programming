09727af0fa4ca47765ec7e71825053093bd1ae1a
stephenchuks <stephenchuks@uopeople.edu>
2022-06-07 10:13:14 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * main - print lowercase alphabets
+  *
+  * Return: void
+  */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
