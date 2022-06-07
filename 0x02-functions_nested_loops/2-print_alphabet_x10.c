e10de91455c361bd7464fea799499718d9d21fb8
stephenchuks <stephenchuks@uopeople.edu>
2022-06-07 10:23:46 -0700

    print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * Printing an alphabet ten times in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
